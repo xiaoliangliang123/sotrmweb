4d71d8981747195171a98cc6d5b24a2eb46b84a8
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-08-10 11:03:21 +0800

    创建项目

---
+++ src/main/java/com/storm/stormweb/StormwebApplication.java
@@ -0,0 +1,13 @@
+package com.storm.stormweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StormwebApplication.class, args);
+    }
+
+}
